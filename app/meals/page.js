5b35374d6d2f5f81d1e5c6b6632248670f94046c
alexbotha <alexbotha@hotmail.co.uk>
2024-01-16 21:05:10 +0000

    update

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,15 +1,34 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+import MealItem from "@/components/meals/meal-item";
+
+async function Meal() {
+  const meals = await getMeals();
+
+  return <MealItem meals={meals} />;
+}
 
 export default function Meals() {
   return (
     <>
-      <h1>Meals</h1>
-      <p>
-        <Link href="/meals/share">Check out meal shares</Link>
-      </p>
-      <p>
-        <Link href="/meals/dynamic">Check out dynamic</Link>
-      </p>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highligh}>by you</span>
+        </h1>
+        <p>Choose your favourite!</p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite here</Link>
+        </p>
+        <main className={classes.main}>
+          <Suspense fallback={<p className={classes.loading}>Loading</p>}>
+            <Meal />
+          </Suspense>
+        </main>
+      </header>
     </>
   );
 }
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1 className="not-found">Not found.</h1>
+      <p className="not-found">Could not find it </p>
+    </main>
+  );
+}
